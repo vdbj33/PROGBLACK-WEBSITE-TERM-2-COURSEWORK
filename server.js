23c07de6af16cbb0db12422c16b4ad4cf1ad92a5
Nina Lane <vdbj33@durham.ac.uk>
2026-01-03 23:03:15 +0000

    added bg image and navbar text

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 // use express package, it's easier
+
+// Adapted from the nodejs lecture notes
+const express = require("express")
+const app = express
+const port = 3000
+
+app.get("/", function(req,resp){
+    resp.send("Hello world")
+})
+
+app.listen(port)
