4c97b23af9305c2c69dd886c4fdf08806ce3f010
Nina Lane <vdbj33@durham.ac.uk>
2026-01-04 23:22:17 +0000

    got server workin

---
+++ client.js
@@ -0,0 +1,11 @@
+const app = document.getElementById("app")
+
+function renderHome(){
+    app.innerHTML = `
+    <h1>Kpop Quiz!!!</h1>
+    <p>Welcome to my Kpop Quiz website!</p>
+    <p>This website includes information about a range of Kpop groups alongside the quiz, check it out using the navbar at the top!</p>
+    `
+}
+
+renderHome()
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // use express package, it's easier
 
 // Adapted from the nodejs lecture notes
+const path = require("path")
 const express = require("express")
-const app = express
+const app = express()
 const port = 3000
 
-app.get("/", function(req,resp){
-    resp.send("Hello world")
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "/index.html"))   // Send the HTML file
+    app.use(express.static(__dirname))      // Send the other files (CSS, JS, media)
 })
 
 app.listen(port)
+console.log("Server at http://localhost:" + port)
